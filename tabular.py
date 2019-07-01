88723c2dda704d0126482de3fb4f50a93d59b194
sapana subedi <subedisapana10@gmail.com>
2019-07-02 01:41:43 +0545

    converting dataset into table format

---
+++ tabular.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# In[15]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+#imports
+import pandas as pd
+
+
+# read csv in a data frame
+df=pd.read_csv("Desktop/juypter/house_data.csv", sep=",")
+df
+
